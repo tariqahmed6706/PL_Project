385abd1a45517f7d4be9f0d9f039154c8cf38d8f
tariqahmed6706 <tariq.ahmed6706@gmail.com>
2025-11-30 12:18:09 +0200

    kkk

--- exit.java
+++ exit.java
@@ -1,5 +1,6 @@
 public class exit {
     public static void main(String[] args) {
+        System.out.println("Program is running..." );
         System.out.println("Exiting the program.");
         System.exit(0);
     }
