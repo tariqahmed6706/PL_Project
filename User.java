0535c6f8049ebc831b9c119327297214b960d038
eslam <206743871+eslammohamed500@users.noreply.github.com>
2025-12-12 00:16:10 +0200

    Create User class for user management

---
+++ User.java
@@ -0,0 +1,43 @@
+package usermanagement;
+
+public class User {
+    private String id;
+    private String name;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String id, String name, String username, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String toFileString() {
+        return id + "," + name + "," + username + "," + password + "," + role;
+    }
+
+    public static User fromFileString(String line) {
+        String[] data = line.split(",");
+        if (data.length < 5) return null; 
+        return new User(data[0], data[1], data[2], data[3], data[4]);
+    }
+
+    public String getId() {
+        return id; 
+    }
+    public String getName() { 
+        return name;
+    }
+    public String getUsername() { 
+        return username; 
+    }
+    public String getPassword() { 
+        return password; 
+    }
+    public String getRole() { 
+        return role; 
+    }
+}
